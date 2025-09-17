17a7a65b3d7822cd4efb13554adf54239f1789b1
Settings <bookdjspindim@gmail.com>
2025-09-17 14:22:15 +0200

    anytype

--- flow/repository/flowRepo.go
+++ flow/repository/flowRepo.go
@@ -1,1 +1,19 @@
 package repository
+
+import "anytype-flow-crud/flow/entities"
+
+type FlowRepo struct {
+	BaseURL string
+	APIKEY  string
+}
+
+func NewFlowRepo() *FlowRepo {
+	return &FlowRepo{
+		BaseURL: "http://localhost:3000/v1",
+		APIKEY:  "ud044Ju8oDxNnni/BGplsR/3LKmqKSMEuyLdvb3y6aE=",
+	}
+}
+
+func (r FlowRepo) ExposeNewContent(content entities.Content) error {
+
+}

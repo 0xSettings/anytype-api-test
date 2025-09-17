f6d97bd9dacb851dcb34641432fb091d02d32fb2
Settings <bookdjspindim@gmail.com>
2025-09-17 14:53:56 +0200

    anytype

--- flow/service/contentService.go
+++ flow/service/contentService.go
@@ -1,1 +1,20 @@
 package service
+
+import (
+	"anytype-flow-crud/flow/entities"
+	"anytype-flow-crud/flow/repository"
+)
+
+type ContentService struct {
+	rep *repository.FlowRepo
+
+	//Since API isnt supporting Update via body params
+	cache map[string]*entities.Content
+}
+
+func ExposeNewContentService(rep *repository.FlowRepo) *ContentService {
+	return &ContentService{
+		rep:   rep,
+		cache: make(map[string]*entities.Content),
+	}
+}
